1eeaaeb9ff9197c801773abec743b33341d0cd3c
Kittisak Maneewong <kittisak.m162@gmail.com>
2023-07-11 08:31:42 +0700

    feat: add main bg dark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { ApplyLoan } from '@/components'
 
 const Home = () => {
   return (
-    <main className="bg-white py-12">
-      <section className="bg-white dark:bg-gray-900">
+    <main className="bg-white dark:bg-gray-900">
+      <section className="bg-white dark:bg-gray-900 py-12">
         <div className="container mx-auto px-4">
           <ApplyLoan />
         </div>
