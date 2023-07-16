cae41dbf673a1949fbe1492362fa41dec2aa2597
Kittisak Maneewong <kittisak.m162@gmail.com>
2023-07-16 16:53:54 +0700

    feat: update db to sqlite

--- prisma/migrations/20230716095217_init/migration.sql
+++ prisma/migrations/20230716095217_init/migration.sql
@@ -1,13 +1,11 @@
 -- CreateTable
 CREATE TABLE "Blog" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "image" TEXT NOT NULL,
     "shortContent" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
